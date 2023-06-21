19cda2c00b54c2f3820ed64cff68681ee6487785
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-06-21 12:57:31 +0200

    css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import Header from './Header/Header';
 
